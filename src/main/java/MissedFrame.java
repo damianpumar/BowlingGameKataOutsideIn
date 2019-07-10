1926c4e274840a6c3f866438a5089b6c4d93400b
Damián Pumar <damianpumar@gmail.com>
2019-07-10 15:04:35 +0200

    Scores is 7 when frame has miss symbol on left the frame Test Green

--- src/main/java/MissedFrame.java
+++ src/main/java/MissedFrame.java
@@ -10,6 +10,9 @@ public class MissedFrame extends Frame {
         if (this.pins[1].isMiss())
             return this.pins[0].number();
 
+        if (this.pins[0].isMiss())
+            return this.pins[1].number();
+
         return 0;
     }
 }
