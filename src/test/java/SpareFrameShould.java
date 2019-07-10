52ac3c5864db3cedb040532ccc3a317fdf92191a
Damián Pumar <damianpumar@gmail.com>
2019-07-10 13:04:22 +0200

    SpareFrame return 10 when the next pin is missed Test Red

--- src/test/java/SpareFrameShould.java
+++ src/test/java/SpareFrameShould.java
@@ -9,4 +9,11 @@ public class SpareFrameShould {
 
         assertThat(spareFrame.score()).isEqualTo(12);
     }
+
+    @Test
+    public void return_10_when_the_next_pin_is_missed() {
+        SpareFrame spareFrame = new SpareFrame(new Pin("-"));
+
+        assertThat(spareFrame.score()).isEqualTo(10);
+    }
 }
