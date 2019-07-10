2f87ef93c30ffb73a8656b3965f40a086ee21a3d
Damián Pumar <damianpumar@gmail.com>
2019-07-10 14:51:52 +0200

    SpareParser return the correct score Test Red

--- src/test/java/SpareParserShould.java
+++ src/test/java/SpareParserShould.java
@@ -24,4 +24,37 @@ public class SpareParserShould {
 
         assertThat(frame).isNotNull();
     }
+
+    @Test
+    public void return_12_when_the_next_pin_is_2() {
+        String[] rolls = new String[]{"4/", "2-", "X", "X", "X", "X", "X", "X", "X", "X", "X",};
+
+        SpareParser spareParser = new SpareParser(rolls);
+
+        Frame frame = spareParser.evaluate(0);
+
+        assertThat(frame.score()).isEqualTo(12);
+    }
+
+    @Test
+    public void return_10_when_the_next_pin_is_missed() {
+        String[] rolls = new String[]{"4/", "-8", "X", "X", "X", "X", "X", "X", "X", "X", "X",};
+
+        SpareParser spareParser = new SpareParser(rolls);
+
+        Frame frame = spareParser.evaluate(0);
+
+        assertThat(frame.score()).isEqualTo(10);
+    }
+
+    @Test
+    public void return_20_when_the_next_pin_is_strike() {
+        String[] rolls = new String[]{"4/", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",};
+
+        SpareParser spareParser = new SpareParser(rolls);
+
+        Frame frame = spareParser.evaluate(0);
+
+        assertThat(frame.score()).isEqualTo(20);
+    }
 }
