8a537bd491d2adc8dfc1cee1861d12d73c6bbd0d
Damián Pumar <damianpumar@gmail.com>
2019-07-10 15:37:57 +0200

    Add acceptance Test Green

--- src/main/java/ParserBuilder.java
+++ src/main/java/ParserBuilder.java
@@ -1,11 +1,24 @@
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class ParserBuilder {
     private static final String FRAME_BOUNDARY = "|";
 
     public Parser build(String rolls) {
-        String[] rollsParsed = rolls.split(Pattern.quote(FRAME_BOUNDARY));
+        String[] rollsParsed = this.parse(rolls);
 
         return new Parser(rollsParsed);
+    }
+
+    private String[] parse(String rolls) {
+        ArrayList<String> parsed = new ArrayList();
+
+        for (String roll : rolls.split(Pattern.quote(FRAME_BOUNDARY))) {
+            if (roll.equals("")) continue;
+
+            parsed.add(roll);
+        }
+
+        return parsed.toArray(new String[]{});
     }
 }
