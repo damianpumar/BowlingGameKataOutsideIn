cda630f1ac24772ef6089eed0da01929ad1a4d5c
Damián Pumar <damianpumar@gmail.com>
2019-07-10 15:37:37 +0200

    Add acceptance Test Red

---
+++ src/test/java/BowlingFeature.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BowlingFeature {
+    private Bowling bowling;
+
+    @Before
+    public void setup() {
+        this.bowling = new Bowling(new LineParser(new ParserBuilder()));
+    }
+
+    @Test
+    public void scores_is_300_when_all_pin_is_strike() {
+        String rolls = "X|X|X|X|X|X|X|X|X|X||XX";
+
+        assertThat(this.bowling.score(rolls)).isEqualTo(300);
+    }
+}
