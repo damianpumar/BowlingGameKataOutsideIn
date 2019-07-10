26b5dc29fb85e788029298060dd42549395204cb
Damián Pumar <damianpumar@gmail.com>
2019-07-10 12:25:06 +0200

    Refactor

--- src/test/java/PinShould.java
+++ src/test/java/PinShould.java
@@ -16,4 +16,39 @@ public class PinShould {
 
         assertThat(pin.isStrike()).isFalse();
     }
+
+    @Test
+    public void true_when_is_Spare() {
+        Pin pin = new Pin("/");
+
+        assertThat(pin.isSpare()).isTrue();
+    }
+
+    @Test
+    public void false_when_is_not_Spare() {
+        Pin pin = new Pin("5");
+
+        assertThat(pin.isSpare()).isFalse();
+    }
+
+    @Test
+    public void true_when_is_a_number() {
+        Pin pin = new Pin("5");
+
+        assertThat(pin.isNumber()).isTrue();
+    }
+
+    @Test
+    public void false_when_is_not_a_number() {
+        Pin pin = new Pin("X");
+
+        assertThat(pin.isNumber()).isFalse();
+    }
+
+    @Test
+    public void be_5_when_a_number_is_5() {
+        Pin pin = new Pin("5");
+
+        assertThat(pin.number()).isEqualTo(5);
+    }
 }
