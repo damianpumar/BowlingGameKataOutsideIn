3e621791619ffd0da2df0daf0da9a3e821466ee5
Damián Pumar <damianpumar@gmail.com>
2019-07-10 15:15:32 +0200

    NumberParser return the correct score Test Red

--- src/test/java/NumberParserShould.java
+++ src/test/java/NumberParserShould.java
@@ -24,4 +24,26 @@ public class NumberParserShould {
 
         assertThat(frame).isNotNull();
     }
+
+    @Test
+    public void score_is_9_when_frame_has_8_and_1() {
+        String[] rolls = new String[]{"81", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",};
+
+        NumberParser numberParser = new NumberParser(rolls);
+
+        Frame frame = numberParser.evaluate(0);
+
+        assertThat(frame.score()).isEqualTo(9);
+    }
+
+    @Test
+    public void score_is_4_when_frame_has_3_and_1() {
+        String[] rolls = new String[]{"31", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",};
+
+        NumberParser numberParser = new NumberParser(rolls);
+
+        Frame frame = numberParser.evaluate(0);
+
+        assertThat(frame.score()).isEqualTo(4);
+    }
 }
