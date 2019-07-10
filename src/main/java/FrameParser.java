2011a503e09bd7fa88a26e02eadc0c8891f5c523
Damián Pumar <damianpumar@gmail.com>
2019-07-10 12:47:26 +0200

    StrikeParser should create StrikeFrame with next two pins Test Green

--- src/main/java/FrameParser.java
+++ src/main/java/FrameParser.java
@@ -18,4 +18,14 @@ public abstract class FrameParser {
     protected abstract boolean isMatch(String currentFrame);
 
     protected abstract Frame createFrame(String[] rollsParsed, String currentFrame, int positionToAnalyze);
+
+    protected Pin[] createPin(String frame) {
+        String[] frames = frame.split("");
+
+        if (frames.length > 1) {
+            return new Pin[]{new Pin(frames[0]), new Pin(frames[1])};
+        }
+
+        return new Pin[]{new Pin(frames[0])};
+    }
 }
--- src/main/java/StrikeParser.java
+++ src/main/java/StrikeParser.java
@@ -10,8 +10,11 @@ public class StrikeParser extends FrameParser {
 
     @Override
     protected Frame createFrame(String[] rollsParsed, String currentFrame, int positionToAnalyze) {
-        return new StrikeFrame(
-                new Pin(rollsParsed[positionToAnalyze + 1]),
-                new Pin(rollsParsed[positionToAnalyze + 2]));
+        Pin[] pinsOnNextFrame = this.createPin(rollsParsed[positionToAnalyze + 1]);
+
+        if (pinsOnNextFrame.length > 1)
+            return new StrikeFrame(pinsOnNextFrame);
+
+        return new StrikeFrame(pinsOnNextFrame[0], this.createPin(rollsParsed[positionToAnalyze + 2])[0]);
     }
 }
