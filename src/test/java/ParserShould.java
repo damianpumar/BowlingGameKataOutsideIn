dea611ce8e95fbac14ba46d3e9a637751faace58
Damián Pumar <damianpumar@gmail.com>
2019-07-18 10:40:54 +0200

    Added codecov configuration

--- src/test/java/ParserShould.java
+++ src/test/java/ParserShould.java
@@ -13,4 +13,37 @@ public class ParserShould {
 
         assertThat(frame).isInstanceOf(StrikeFrame.class);
     }
+
+    @Test
+    public void create_instance_of_SpareFrame_when_rolls_contain_Strike() {
+        String[] rolls = new String[]{"5/", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",};
+
+        Parser parser = new Parser(rolls);
+
+        Frame frame = parser.evaluate(0);
+
+        assertThat(frame).isInstanceOf(SpareFrame.class);
+    }
+
+    @Test
+    public void create_instance_of_MissedFrame_when_rolls_contain_Strike() {
+        String[] rolls = new String[]{"5-", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",};
+
+        Parser parser = new Parser(rolls);
+
+        Frame frame = parser.evaluate(0);
+
+        assertThat(frame).isInstanceOf(MissedFrame.class);
+    }
+
+    @Test
+    public void create_instance_of_NumberFrame_when_rolls_contain_Strike() {
+        String[] rolls = new String[]{"51", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X",};
+
+        Parser parser = new Parser(rolls);
+
+        Frame frame = parser.evaluate(0);
+
+        assertThat(frame).isInstanceOf(NumberFrame.class);
+    }
 }
